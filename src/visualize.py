e1c98de15a286764d0b1c7efe37b45f67b1d4008
Eshaan-Lumba <eshaan100102@gmail.com>
2024-02-04 15:19:26 -0800

    Added Korean font functionality

--- src/visualize.py
+++ src/visualize.py
@@ -11,6 +11,11 @@ args = parser.parse_args()
 # imports
 import os
 import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+font = fm.FontProperties(fname='/home/elaa2020/bigdata/hw/hw2/twitter_coronavirus/TTF/NanumSquareNeo-aLt.ttf')
 from collections import Counter,defaultdict
 
 # open the input path
@@ -22,7 +27,16 @@ if args.percent:
     for k in counts[args.key]:
         counts[args.key][k] /= counts['_all'][k]
 
-# print the count values
+# stores count values
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+top_10 = items[:10]
+sorted_top_10 = sorted(top_10, key=lambda item: item[1])
+k, v = zip(*sorted_top_10)
+title = args.key
+x_label = args.input_path.split('.')[1].upper()
+
+plt.bar(range(len(k)), v, tick_label=k)
+plt.ylabel("Tweets")
+plt.xlabel(x_label)
+plt.title(f"Top 10 {x_label} with Tweets containing {title} in 2020", fontproperties = font)
+plt.savefig(f"{x_label}_{title}.png")
