d1c412926b5996d3af8be6815c4773d9ca85c1e4
Eshaan-Lumba <eshaan100102@gmail.com>
2024-02-04 23:06:35 -0800

    Added alternative_reduce.py

---
+++ src/alternative_reduce.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# command line args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--hashtags',nargs='+',required=True)
+args = parser.parse_args()
+
+# imports
+import os
+import json
+import glob
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from collections import Counter,defaultdict
+
+total = {}
+file_pat = "/home/elaa2020/bigdata/hw/hw2/twitter_coronavirus/outputs/geoTwitter20-*.lang"
+files = glob.glob(file_pat)
+for file in files:
+    with open(file) as f:
+        date = os.path.basename(file)[10:18]
+        tmp = json.load(f)
+        for hsh in args.hashtags:
+            if hsh in tmp:
+                for language in tmp[hsh]:
+                    if (date, hsh) not in total:
+                        total[(date, hsh)] = 0
+                    
+                    total[(date, hsh)] += tmp[hsh][language]
+
+total = dict(sorted(total.items(), key = lambda key: key[0]))
+for hsh in args.hashtags:
+    dates = []
+    counts = []
+
+    for k in total:
+        if k[1] == hsh:
+            dates.append(k[0])
+            counts.append(total[k])
+
+    plt.plot(dates, counts, label=hsh)
+
+plt.xlabel('Date')
+plt.ylabel('Tweets')
+plt.title('Daily tweets for provided hashtags')
+plt.legend()
+
+plt.savefig(f'line_graph_{args.hashtags}.png')
